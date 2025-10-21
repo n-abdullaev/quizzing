89069122a8e40ed0d7a180e4c5f2e68754f03f8d
Nurmukhammed <imnurmukhammed@gmail.com>
2025-10-20 20:54:29 -0500

    added home button

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -9,21 +9,34 @@ export default function Catalog() {
     navigate("/quiz", { state: { quiz: quizFile } });
   };
 
-  return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center gap-6">
-      <h1 className="text-2xl font-semibold">Choose a Quiz</h1>
+  const toHome = () => {
+    navigate("/");
+  }
 
-      <div className="flex flex-col gap-3 w-full max-w-md">
-        {data.quizzes.map((opt, i) => (
-          <button
-            key={i}
-            className="border rounded-lg py-2 bg-blue-600 text-white hover:bg-blue-700"
-            onClick={() => startQuiz(opt.file)}
-          >
-            {opt.title}
-          </button>
-        ))}
+  return (
+    <>
+
+      <button className="border rounded-lg px-4 py-2 
+          bg-blue-600 margin m-4 text-white"
+          onClick={toHome}
+      >
+        home
+      </button>
+      <div className="min-h-screen w-full flex flex-col items-center justify-center gap-6">
+        <h1 className="text-2xl font-semibold">Choose a Quiz</h1>
+
+        <div className="flex flex-col gap-3 w-full max-w-md">
+          {data.quizzes.map((opt, i) => (
+            <button
+              key={i}
+              className="border rounded-lg py-2 bg-blue-600 text-white hover:bg-blue-700"
+              onClick={() => startQuiz(opt.file)}
+            >
+              {opt.title}
+            </button>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
